9d3bcf913f87e314021830aa37a65e377d476f41
SubramanyaNayak <subramanyanayak3@gmail.com>
2024-02-05 00:09:23 +0530

    data transformation added

--- test.py
+++ test.py
@@ -6,9 +6,14 @@ from src.logger import logger
 from dataclasses import dataclass
 
 from src.components.data_ingestion import DataIngestionConfig,DataIngestion
+from src.components.data_transformation import DataTransformationConfig,DataTransformation
 
 
 
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
